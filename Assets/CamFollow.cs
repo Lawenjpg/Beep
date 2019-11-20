2896dbe1413c2dae8c4518a2a727ac1c9e975e24
Cody Santana <CodyLawrence2014@gmail.com>
2019-11-20 16:45:39 -0500

    Added Project Files

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    GameObject PlayerObj;
+    Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerObj = GameObject.Find("RattleSnake");
+        cameraOffset = new Vector3(0, 2, -3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerObj.transform.position + cameraOffset;
+    }
+}
---
+++ Assets/Movement1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement1 : MonoBehaviour
+{
+    public Vector3 pos = new Vector3(0f, 0f, 0f);
+    public Transform RattleSnake;
+    public float tileamount = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RattleSnake.position += new Vector3(-tileamount, 0f, 0f);
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RattleSnake.position += new Vector3(tileamount, 0f, 0f);
+
+
+
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            RattleSnake.position -= new Vector3(0f, 0f, tileamount);
+
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            RattleSnake.position -= new Vector3(0f, 0f, -tileamount);
+        }
+
+    }
+}
